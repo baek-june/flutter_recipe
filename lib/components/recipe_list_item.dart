cc521bfce3c277a573bb11fa1ba4f4094a025459
baek-jun <toypiano@naver.com>
2023-11-14 10:33:57 +0900

    실제 아이폰 연결설정 및 레시피앱 파일 추가

---
+++ lib/components/recipe_list_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem(this.imageName, this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
